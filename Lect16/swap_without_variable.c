2fe4675489ea21c5400b2044a03842da9bd07052
LimboSVD <chowdhuryamitesh2006@gmail.com>
2025-10-05 21:39:36 +0530

    2D-Array

---
+++ Lect16/swap_without_variable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int swap(int *x, int *y){
+    *x=*x+*y;
+    *y=*x-*y;
+    *x=*x-*y;
+    printf("%d,%d",*x,*y);
+}
+
+
+int main(){
+    int x=10;
+    int y=20;
+    swap(&x,&y);
+ 
+}
